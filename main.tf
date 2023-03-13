d01e0ab49f37414d861c27c983a940946f3257c9
sirellvenyao <127486421+sirellvenyao@users.noreply.github.com>
2023-03-13 14:40:22 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_vpc" "default" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  lifecycle {
+    prevent_destroy = true
+  }
+  tags = {
+    Name = "${var.vpc_name}-vpc"
+    created-by = var.created-by
+    owner = var.owner
+    business-line = var.business-line
+  }
+}
+
+
 resource "aws_security_group" "endpoint_shared" {
   vpc_id = aws_vpc.default.id
   name = "endpoint_shared"
