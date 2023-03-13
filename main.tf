8b2e66784150690cbba84ca7018bee8dbb1954a6
sirellvenyao <127486421+sirellvenyao@users.noreply.github.com>
2023-03-13 14:53:38 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,31 +16,197 @@ resource "aws_vpc" "default" {
   }
 }
 
+module "monitor_shared_sg" {
 
-resource "aws_security_group" "endpoint_shared" {
-  vpc_id = aws_vpc.default.id
-  name = "endpoint_shared"
-  description = "Common endpoints security group."
-  lifecycle {
-    prevent_destroy = true
-  }
-  ingress {
-    cidr_blocks = [var.vpc_cidr]
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-    self = true
-  }
-  egress {
-    cidr_blocks = ["0.0.0.0/0"]
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
-  }
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "vaex-monitor-shared"
+  description = "The shared sg for monitor resources."
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
+
+  ingress_rules = [
+
+    "all-all",
+  ]
+
+  ingress_cidr_blocks = [
+
+    var.xygj_private_ip,
+    "10.201.37.250/32",
+    "10.200.14.72/32",
+    "10.200.12.56/32",
+    "10.200.13.179/32",
+    "10.200.14.247/32",
+
+  ]
+
+  ingress_with_cidr_blocks = [
+
+
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "for aws health check and other app"
+      cidr_blocks = "10.201.0.0/16"
+    },
+
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "for aws health check and other app"
+      cidr_blocks = "10.201.0.0/16"
+    },
+
+    {
+      from_port   = 30000
+      to_port     = 35000
+      protocol    = "tcp"
+      description = "for aws health check and other app"
+      cidr_blocks = "10.201.0.0/16"
+    },
+    
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "for Cyberark HK jumper server"
+      cidr_blocks = "10.200.2.166/32"
+    },    
+    
+    
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "for risk nginx"
+      cidr_blocks = "10.202.25.233/32"
+    },   
+    
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "for risk nginx"
+      cidr_blocks = "10.202.25.233/32"
+    },       
+
+    {
+      from_port   = 31368
+      to_port     = 31368
+      protocol    = "tcp"
+      description = "financial for cyberark"
+      cidr_blocks = "10.200.2.166/32"
+    },       
+    
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "for financial nginx"
+      cidr_blocks = "10.200.12.56/32"
+    },  
+    
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "for financial nginx"
+      cidr_blocks = "10.200.13.179/32"
+    },     
+    
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      description = "for risk xygj windows"
+      cidr_blocks = "10.200.14.29/32"
+    },      
+
+  ]
+
+
+  ingress_with_source_security_group_id = [
+
+    {
+
+      from_port                = 18000
+      to_port                  = 18000
+      protocol                 = "tcp"
+      source_security_group_id = module.internal_elb_shared_sg.security_group_id
+      description              = "allow internal-elb traffic to monitor"
+
+    },
+
+    {
+
+      from_port                = 19000
+      to_port                  = 19000
+      protocol                 = "tcp"
+      source_security_group_id = module.internal_elb_shared_sg.security_group_id
+      description              = "allow internal-elb traffic to monitor"
+
+    },
+
+    {
+
+      from_port                = 8480
+      to_port                  = 8480
+      protocol                 = "tcp"
+      source_security_group_id = module.internal_elb_shared_sg.security_group_id
+      description              = "allow internal-elb traffic to monitor"
+
+    },
+
+    {
+
+      from_port                = 8481
+      to_port                  = 8481
+      protocol                 = "tcp"
+      source_security_group_id = module.internal_elb_shared_sg.security_group_id
+      description              = "allow internal-elb traffic to monitor"
+
+    },
+
+    {
+
+      from_port                = 9000
+      to_port                  = 9000
+      protocol                 = "tcp"
+      source_security_group_id = module.internal_elb_shared_sg.security_group_id
+      description              = "allow internal-elb traffic to monitor"
+
+    },
+    
+    {
+
+      from_port                = 5100
+      to_port                  = 5100
+      protocol                 = "tcp"
+      source_security_group_id = module.ops_shared_sg.security_group_id
+      description              = "allow ops traffic to monitor"
+
+    },    
+  ]
+
+  ingress_with_self = [
+    {
+      rule        = "all-all"
+      description = "Self"
+    },
+  ]
+
+  egress_rules = ["all-all"]
+
+  egress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_ipv6_cidr_blocks = []
+
   tags = {
-    Name = "endpoint_shared"
-    created-by = var.created-by
-    owner = var.owner
+    Name          = "vaex-monitor-shared"
+    created-by    = var.created-by
+    owner         = var.owner
     business-line = var.business-line
   }
 }
