9389625d4d8866592f8a8c012f779fb620fbb254
sirellvenyao <sirellvenyao@gmail.com>
2023-03-20 14:11:20 +0700

    m,an

--- main.tf
+++ main.tf
@@ -2,147 +2,118 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "default" {
-  cidr_block           = var.vpc_cidr
+resource "aws_vpc" "tf_vpc2" {
+  cidr_block           = "10.222.0.0/16"
   enable_dns_hostnames = true
-  lifecycle {
-    prevent_destroy = true
-  }
+  enable_dns_support   = true
+
   tags = {
-    Name = "${var.vpc_name}-vpc"
-    created-by = var.created-by
-    owner = var.owner
-    business-line = var.business-line
+    Name = "tf_test"
+  }
+
+}
+
+resource "aws_subnet" "tf_a_new_one" {
+  vpc_id                  = aws_vpc.tf_vpc2.id
+  cidr_block              = "10.222.10.0/26"
+  availability_zone       = "ap-east-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "tf_a_new_one"
   }
 }
 
-module "monitor_shared_sg" {
-
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "vaex-monitor-shared"
-  description = "The shared sg for monitor resources."
-  vpc_id      = aws_vpc.default.id
-
-  ingress_rules = [
-
-    "all-all",
-  ]
-
-  ingress_cidr_blocks = [
-
-    var.xygj_private_ip,
-    "10.201.37.250/32",
-    "10.200.14.72/32",
-    "10.200.12.56/32",
-    "10.200.13.179/32",
-    "10.200.14.247/32",
-
-  ]
-
-  ingress_with_cidr_blocks = [
-
-
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "for aws health check and other app"
-      cidr_blocks = "10.201.0.0/16"
-    },
-
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "for aws health check and other app"
-      cidr_blocks = "10.201.0.0/16"
-    },
-
-    {
-      from_port   = 30000
-      to_port     = 35000
-      protocol    = "tcp"
-      description = "for aws health check and other app"
-      cidr_blocks = "10.201.0.0/16"
-    },
-    
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "for Cyberark HK jumper server"
-      cidr_blocks = "10.200.2.166/32"
-    },    
-    
-    
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "for risk nginx"
-      cidr_blocks = "10.202.25.233/32"
-    },   
-    
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "for risk nginx"
-      cidr_blocks = "10.202.25.233/32"
-    },       
-
-    {
-      from_port   = 31368
-      to_port     = 31368
-      protocol    = "tcp"
-      description = "financial for cyberark"
-      cidr_blocks = "10.200.2.166/32"
-    },       
-    
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "for financial nginx"
-      cidr_blocks = "10.200.12.56/32"
-    },  
-    
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "for financial nginx"
-      cidr_blocks = "10.200.13.179/32"
-    },     
-    
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "for risk xygj windows"
-      cidr_blocks = "10.200.14.29/32"
-    },      
-
-  ]
-
-  ingress_with_self = [
-    {
-      rule        = "all-all"
-      description = "Self"
-    },
-  ]
-
-  egress_rules = ["all-all"]
-
-  egress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_ipv6_cidr_blocks = []
+resource "aws_subnet" "tf_a_new" {
+  vpc_id                  = aws_vpc.tf_vpc2.id
+  cidr_block              = "10.222.11.0/24"
+  availability_zone       = "ap-east-1c"
+  map_public_ip_on_launch = false
 
   tags = {
-    Name          = "vaex-monitor-shared"
-    created-by    = var.created-by
-    owner         = var.owner
-    business-line = var.business-line
+    Name = "tf_a_new_one"
   }
+}
+
+
+resource "aws_security_group" "tf_test" {
+  vpc_id = aws_vpc.tf_vpc2.id
+  name = "endpoint_shared"
+  description = "Common endpoints security group."
+  lifecycle {
+    prevent_destroy = true
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    self = true
+  }
+  ingress {
+    cidr_blocks = ["10.0.0.0/16"]
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    self = true
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+  }
+  ingress {
+    cidr_blocks = ["10.2.0.0/16"]
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    self = true
+  }  
+  tags = {
+    Name = "endpoint_shared"
+    business-line = "test"
+  }
+}
+
+resource "aws_security_group" "sentinel_22_added_sg" {
+  vpc_id = aws_vpc.tf_vpc2.id
+  name = "sentinel_22_added_sg"
+  description = "sentinel_22_added_sg"
+  lifecycle {
+    prevent_destroy = true
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+  }
+  ingress {
+    cidr_blocks = ["10.3.0.0/16"]
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    self = true
+  }
+  ingress {
+    cidr_blocks = ["10.3.0.0/16"]
+    from_port = 100
+    protocol  = "-1"
+    to_port   = 100
+    self = true
+  }    
+  tags = {
+    Name = "sentinel_22_added_sg"
+    business-line = "test"
+  }
+}
+
+resource "aws_security_group_rule" "just_test" {
+  type              = "ingress"
+  from_port         = 161
+  to_port           = 161
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0","10.3.0.0/24"]
+  security_group_id = aws_security_group.sentinel_22_added_sg.id
 }
