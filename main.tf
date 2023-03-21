6d49d5cf5854fa6061491fef8a0ce5e0863139bf
sirellvenyao <sirellvenyao@gmail.com>
2023-03-21 15:16:19 +0700

    commit

--- main.tf
+++ main.tf
@@ -153,3 +153,29 @@ resource "aws_instance" "correct_instance" {
       instance_metadata_tags      = "enabled"
     } 
 } 
+
+resource "aws_network_acl" "tf_nacl" {
+  vpc_id = aws_vpc.tf_vpc2.id
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "10.3.0.0/18"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "10.3.0.0/18"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
