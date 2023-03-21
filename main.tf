0f42f41274717925d52969b58dd0f9ff7fad668a
sirellvenyao <sirellvenyao@gmail.com>
2023-03-21 17:22:05 +0700

    commit

--- main.tf
+++ main.tf
@@ -179,3 +179,10 @@ resource "aws_network_acl" "tf_nacl" {
     Name = "main"
   }
 }
+
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.tf_vpc2.id
+}
