f52fe17041abc0a1ba4127d935eda0f87f050ecb
Ellven Yao <Ellven.Yao@flashdot.com>
2023-04-21 14:47:38 +0800

    Commit

--- main.tf
+++ main.tf
@@ -226,3 +226,15 @@ resource "aws_elb" "wu-tang" {
     Name = "wu-tang"
   }
 }
+
+
+resource "aws_load_balancer_policy" "wu-tang-root-ca-backend-auth-policy" {
+  load_balancer_name = aws_elb.wu-tang.name
+  policy_name        = "wu-tang-root-ca-backend-auth-policy"
+  policy_type_name   = "BackendServerAuthenticationPolicyType"
+
+  policy_attribute {
+    name  = "PublicKeyPolicyName"
+    value = aws_load_balancer_policy.wu-tang-root-ca-pubkey-policy.policy_name
+  }
+}
