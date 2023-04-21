361eaf69a4556446f94d9eb62314f01535ab9876
Ellven Yao <Ellven.Yao@flashdot.com>
2023-04-21 14:46:26 +0800

    commit

--- main.tf
+++ main.tf
@@ -209,3 +209,20 @@ resource "aws_elasticsearch_domain" "example" {
     Domain = "TestDomain"
   }
 }
+
+resource "aws_elb" "wu-tang" {
+  name               = "wu-tang"
+  availability_zones = ["us-east-1a"]
+
+  listener {
+    instance_port      = 443
+    instance_protocol  = "http"
+    lb_port            = 443
+    lb_protocol        = "https"
+    ssl_certificate_id = "arn:aws:iam::000000000000:server-certificate/wu-tang.net"
+  }
+
+  tags = {
+    Name = "wu-tang"
+  }
+}
