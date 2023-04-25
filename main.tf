061978447fa1cf234c926e5f8cf8491047155fb4
Ellven Yao <Ellven.Yao@flashdot.com>
2023-04-25 16:24:35 +0800

    Commit

--- main.tf
+++ main.tf
@@ -265,3 +265,27 @@ TASK_DEFINITION
     cpu_architecture        = "X86_64"
   }
 }
+
+resource "aws_ecs_task_definition" "ellventest" {
+  family                   = "test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "iis",
+    "image": "mcr.microsoft.com/windows/servercore/iis",
+    "cpu": 1024,
+    "memory": 2048,
+    "essential": true
+  }
+]
+TASK_DEFINITION
+
+  runtime_platform {
+    operating_system_family = "WINDOWS_SERVER_2019_CORE"
+    cpu_architecture        = "X86_64"
+  }
+}
