c487a4d1b6a59832e087f71bf0ac1f0fade13c40
Ellven Yao <Ellven.Yao@flashdot.com>
2023-06-06 15:42:39 +0700

    Commit

--- main.tf
+++ main.tf
@@ -2,15 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
-}
-
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
-
 
 # 1. for Ensure permissions are tightly controlled for AWS ElasticSearch Domains
 
