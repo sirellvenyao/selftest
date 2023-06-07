2f842019f06bd659eab71b457ad8e68ec2e0027b
Ellven Yao <Ellven.Yao@flashdot.com>
2023-06-07 13:30:50 +0700

    LV

--- main.tf
+++ main.tf
@@ -200,43 +200,6 @@ resource "aws_iam_policy" "policy" {
 }
 
 
-# 7. Ensure there are no public file systems for AWS Elastic File System (EFS)
-
-resource "aws_efs_file_system" "fs" {
-  creation_token = "my-product"
-}
-
-data "aws_iam_policy_document" "policy" {
-  statement {
-    sid    = "ExampleStatement01"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "elasticfilesystem:ClientMount",
-      "elasticfilesystem:ClientWrite",
-    ]
-
-    resources = [aws_efs_file_system.fs.arn]
-
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["true"]
-    }
-  }
-}
-
-resource "aws_efs_file_system_policy" "policy" {
-  file_system_id = aws_efs_file_system.fs.id
-  policy         = data.aws_iam_policy_document.policy.json
-}
-
-
 # 8. Ensure Amazon Simple Queue Service (SQS) is not exposed to public
 
 resource "aws_sqs_queue" "terraform_queue" {
