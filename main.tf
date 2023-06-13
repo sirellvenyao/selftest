6dab7cd62c3a1213eabed8b29005d8c5100d05b5
Ellven Yao <Ellven.Yao@flashdot.com>
2023-06-13 10:35:27 +0700

    111

--- main.tf
+++ main.tf
@@ -65,3 +65,29 @@ resource "aws_security_group" "tf_test" {
     business-line = "test"
   }
 }
+
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami = data.aws_ami.this.id
+  instance_market_options {
+    spot_options {
+      max_price = 0.0031
+    }
+  }
+  instance_type = "t4g.nano"
+  tags = {
+    Name = "test-spot"
+  }
+}
