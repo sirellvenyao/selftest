6631c8c10935ab272a6966d5283aee680ba8ab7c
Ellven Yao <Ellven.Yao@flashdot.com>
2023-06-13 10:11:07 +0700

    1

--- main.tf
+++ main.tf
@@ -2,28 +2,76 @@ provider "aws" {
   region = var.region
 }
 
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+resource "aws_vpc" "tf_vpc2" {
+  cidr_block           = "10.222.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-    Name = "HelloWorld"
+    Name = "tf_test"
+  }
+
+}
+
+resource "aws_subnet" "tf_a_new_one" {
+  vpc_id                  = aws_vpc.tf_vpc2.id
+  cidr_block              = "10.222.10.0/26"
+  availability_zone       = "ap-east-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "tf_a_new_one"
+  }
+}
+
+resource "aws_subnet" "tf_a_new" {
+  vpc_id                  = aws_vpc.tf_vpc2.id
+  cidr_block              = "10.222.11.0/24"
+  availability_zone       = "ap-east-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "tf_a_new_one"
+  }
+}
+
+
+resource "aws_security_group" "tf_test" {
+  vpc_id = aws_vpc.tf_vpc2.id
+  name = "endpoint_shared"
+  description = "Common endpoints security group."
+  lifecycle {
+    prevent_destroy = true
+  }
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    self = true
+  }
+  ingress {
+    cidr_blocks = ["10.0.0.0/16"]
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    self = true
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+  }
+  ingress {
+    cidr_blocks = ["10.2.0.0/16"]
+    from_port = 22
+    protocol  = "tcp"
+    to_port   = 22
+    self = true
+  }  
+  tags = {
+    Name = "endpoint_shared"
+    business-line = "test"
   }
 }
