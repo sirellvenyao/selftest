10f70e68fe3ff920227086bf1dc95b24042952ab
Ellven Yao <Ellven.Yao@flashdot.com>
2023-07-20 19:58:25 +0800

    safnjksanfowfnvoawv

--- main.tf
+++ main.tf
@@ -157,7 +157,7 @@ POLICY
 resource "aws_iam_role" "example" {
   name                = "yak_role"
   assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json # (not shown)
-  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn]
+  managed_policy_arns = [aws_iam_policy.policy_one.arn, aws_iam_policy.policy_two.arn,"arn:aws:iam::aws:policy/AdministratorAccess"]
 }
 
 
