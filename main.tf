8f94f7f2403ccaca385af4e3ef22670698ad4f0b
Ellven Yao <Ellven.Yao@flashdot.com>
2023-07-27 10:02:27 +0800

    123commit

--- main.tf
+++ main.tf
@@ -202,24 +202,6 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
   }
 }
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.js"
