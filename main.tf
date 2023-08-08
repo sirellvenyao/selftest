8418fc8c708d98d3d6279fba78e1ccd1dabc57fd
sirellvenyao <sirellvenyao@gmail.com>
2023-08-08 11:07:31 +0800

    ajksfhkajsd

--- main.tf
+++ main.tf
@@ -305,3 +305,32 @@ data "aws_iam_policy_document" "inline_policy" {
     resources = ["*"]
   }
 }
+
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "test"
+  user = aws_iam_user.lb.name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "lb" {
+  user = aws_iam_user.lb.name
+}
